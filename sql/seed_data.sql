6a4a376c2ba125d6afbd20e1197db3e7c94e8b50
Satk <sathishkalisamy88@gmail.com>
2024-10-25 18:50:41 +0530

    commit

--- sql/seed_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO product (product_id, name) VALUES ('P001', 'Product A');
-INSERT INTO product (product_id, name) VALUES ('P002', 'Product B');
-INSERT INTO location (location_id, name) VALUES ('L001', 'Warehouse A');
-INSERT INTO location (location_id, name) VALUES ('L002', 'Warehouse B');
